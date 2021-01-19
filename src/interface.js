e21533964e0e9537c5c328d4122348f431d977ea
xavierloos <xavier.loos04@gmail.com>
2021-01-19 21:30:28 +0000

    Displaying notes

--- src/interface.js
+++ src/interface.js
@@ -2,20 +2,22 @@ function ready(sendNote) {
   if (document.readyState != 'loading') {
     console.log('Please wait for the DOM to load')
     fn()
-
   } else {
     document.addEventListener('DOMContentLoaded', sendNote)
     console.log('DOM has loaded')
   }
 }
 
+var notes = new Notes();
+
 function sendNote() {
-  var notes = new Notes();
   var getNote = document.getElementById("new-note").value
   notes.add(getNote)
-  console.log(notes.notesArray)
   document.getElementById("new-note").value = "" //Clean the input after sending
   event.preventDefault();
+  var createNote = document.createElement("P")
+  createNote.innerText = notes.notesArray.pop()
+  document.getElementById("notes-wrapper").appendChild(createNote);
 }
 
 ready()
