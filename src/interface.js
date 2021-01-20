093ec4c67d260163ca37ff9d1bf7b0c9e4e981db
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-20 12:37:38 +0000

    Refactored Creating List

--- src/interface.js
+++ src/interface.js
@@ -1,23 +1,49 @@
-function ready(sendNote) {
-  if (document.readyState != 'loading') {
-    console.log('Please wait for the DOM to load')
-    fn()
-  } else {
-    document.addEventListener('DOMContentLoaded', sendNote)
-    console.log('DOM has loaded')
-  }
-}
+// function ready(sendNote) {
+//   if (document.readyState != 'loading') {
+//     console.log('Please wait for the DOM to load')
+//
+//   } else {
+//     document.addEventListener('DOMContentLoaded', sendNote)
+//     console.log('DOM has loaded')
+//   }
+// }
 
 var notes = new Notes();
+let button = document.getElementById('create-note')
 
-function sendNote() {
-  var getNote = document.getElementById("new-note").value
-  notes.add(getNote)
-  document.getElementById("new-note").value = "" //Clean the input after sending
-  event.preventDefault();
-  var createNote = document.createElement("P")
-  createNote.innerText = notes.notesArray.pop()
-  document.getElementById("notes-wrapper").appendChild(createNote);
+button.addEventListener("click", function() {
+
+  let area = document.getElementById('new-note')
+  let text = area.value
+  let note = text
+  area.value = ''
+
+  notes.add(note)
+
+  displayNotes()
+})
+
+
+// function sendNote() {
+//   var getNote = document.getElementById("new-note").value
+//   notes.add(getNote)
+//   document.getElementById("new-note").value = "" //Clean the input after sending
+//   event.preventDefault();
+//   var createNote = document.createElement("P")
+//   createNote.innerText = notes.notesArray.pop()
+//   document.getElementById("notes-wrapper").appendChild(createNote);
+// }
+
+// ready()
+
+function displayNotes() {
+  let noteDiv = document.getElementById('notes-wrapper')
+  noteDiv.innerHTML = ''
+
+  notes.getNotesArray().forEach(function(note) {
+    let textElement = document.createElement('p')
+    textElement.textContent = note.substring(0,20) + '...'
+    noteDiv.appendChild(textElement)
+  });
+
 }
-
-ready()
